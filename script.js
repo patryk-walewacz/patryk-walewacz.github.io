b0b5e7df370e9937be4e55b0f0b09ea8272b41e1
patryk-walewacz <patryk24oln@gmail.com>
2017-10-19 12:47:23 +0200

    #

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function createTweet(input) {
 
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
-	if (tweetText.length < 140) {
+	if (tweetText.length > 140) {
     	getQuote();
 	} 
 	else {
